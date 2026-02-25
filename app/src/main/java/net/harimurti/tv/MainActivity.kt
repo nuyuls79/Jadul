54e1d6dec271654f112d86061f817e758b3164c7
nuyuls79 <nuyulshare001@gmail.com>
2026-02-25 17:13:18 +0700

    Update MainActivity.kt

--- app/src/main/java/net/harimurti/tv/MainActivity.kt
+++ app/src/main/java/net/harimurti/tv/MainActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.content.*
 import android.content.pm.ActivityInfo
 import android.os.*
-import android.view.KeyEvent
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -23,11 +22,6 @@ import net.harimurti.tv.extra.*
 import net.harimurti.tv.model.*
 
 open class MainActivity : AppCompatActivity() {
-
-    private var doubleBackToExitPressedOnce = false
-    private var isTelevision = UiMode().isTelevision()
-    private val preferences = Preferences()
-    private val helper = PlaylistHelper()
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: CategoryAdapter
@@ -49,7 +43,6 @@ open class MainActivity : AppCompatActivity() {
         const val REMOVE_FAVORITE = "REMOVE_FAVORITE"
     }
 
-    @SuppressLint("DefaultLocale")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -75,104 +68,40 @@ open class MainActivity : AppCompatActivity() {
         if (!Playlist.cached.isCategoriesEmpty())
             setPlaylistToAdapter(Playlist.cached)
         else
-            showAlertPlaylistError(getString(R.string.null_playlist))
-    }
-
-    private fun setLoadingPlaylist(show: Boolean) {
-        if (show) {
-            binding.loading.startShimmer()
-            binding.loading.visibility = View.VISIBLE
-        } else {
-            binding.loading.stopShimmer()
-            binding.loading.visibility = View.GONE
-        }
+            showAlertPlaylistError("Playlist kosong")
     }
 
     private fun setPlaylistToAdapter(playlistSet: Playlist) {
 
-        if(preferences.sortCategory) playlistSet.sortCategories()
-        if(preferences.sortChannel) playlistSet.sortChannels()
-        playlistSet.trimChannelWithEmptyStreamUrl()
-
-        val fav = helper.readFavorites()
-            .trimNotExistFrom(playlistSet)
-
-        if (preferences.sortFavorite) fav.sort()
-
-        if (fav?.channels?.isNotEmpty() == true)
-            playlistSet.insertFavorite(fav.channels)
-        else playlistSet.removeFavorite()
-
         adapter = CategoryAdapter(playlistSet.categories)
 
-        // kirim klik kategori
         adapter.setOnCategorySelected { category ->
             showChannels(category.channels)
         }
 
         binding.catAdapter = adapter
 
-        Playlist.cached = playlistSet
-        helper.writeCache(playlistSet)
-
-        setLoadingPlaylist(false)
-
-        Toast.makeText(applicationContext, R.string.playlist_updated, Toast.LENGTH_SHORT).show()
-
-        // tampilkan kategori pertama otomatis
+        // tampilkan kategori pertama
         if (playlistSet.categories.isNotEmpty()) {
             showChannels(playlistSet.categories[0].channels)
         }
-
-        if (preferences.playLastWatched && PlayerActivity.isFirst) {
-            val intent = Intent(this, PlayerActivity::class.java)
-            intent.putExtra(PlayData.VALUE, preferences.watched)
-            startActivity(intent)
-        }
     }
 
-    private fun showChannels(channels: List<Channel>) {
-        binding.rvChannels.adapter = ChannelAdapter(channels)
+    private fun showChannels(channels: ArrayList<Channel>?) {
+        binding.rvChannels.adapter =
+            ChannelAdapter(channels, 0, false)
     }
 
     private fun updatePlaylist(useCache: Boolean) {
-        setLoadingPlaylist(true)
-        binding.catAdapter?.clear()
-        val playlistSet = Playlist()
-
-        SourcesReader().set(preferences.sources, object: SourcesReader.Result {
-            override fun onError(source: String, error: String) {
-                val snackbar = Snackbar.make(binding.root, "[${error.uppercase()}] $source", Snackbar.LENGTH_INDEFINITE)
-                snackbar.setAction(android.R.string.ok) { snackbar.dismiss() }
-                snackbar.show()
-            }
-
-            override fun onResponse(playlist: Playlist?) {
-                if (playlist != null) playlistSet.mergeWith(playlist)
-            }
-
-            override fun onFinish() {
-                if (!playlistSet.isCategoriesEmpty()) setPlaylistToAdapter(playlistSet)
-                else showAlertPlaylistError(getString(R.string.null_playlist))
-            }
-        }).process(useCache)
+        setPlaylistToAdapter(Playlist.cached)
     }
 
     private fun showAlertPlaylistError(message: String?) {
-        val alert = AlertDialog.Builder(this).apply {
-            setTitle(R.string.alert_title_playlist_error)
-            setMessage(message)
-            setCancelable(false)
-            setNeutralButton(R.string.settings) { _,_ -> openSettings() }
-            setPositiveButton(R.string.dialog_retry) { _,_ -> updatePlaylist(true) }
-        }
-        alert.create().show()
-    }
-
-    override fun onDestroy() {
-        LocalBroadcastManager.getInstance(this)
-            .unregisterReceiver(broadcastReceiver)
-        super.onDestroy()
+        AlertDialog.Builder(this)
+            .setTitle("Error")
+            .setMessage(message)
+            .setPositiveButton("Retry") { _, _ -> updatePlaylist(true) }
+            .show()
     }
 
     private fun openSettings(){
@@ -181,5 +110,11 @@ open class MainActivity : AppCompatActivity() {
 
     private fun openSearch() {
         SearchDialog().show(supportFragmentManager.beginTransaction(),null)
+    }
+
+    override fun onDestroy() {
+        LocalBroadcastManager.getInstance(this)
+            .unregisterReceiver(broadcastReceiver)
+        super.onDestroy()
     }
 }
