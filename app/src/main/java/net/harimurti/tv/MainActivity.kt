21e279d1ff071621b3e233527f4d111ced54ccd8
nuyuls79 <nuyulshare001@gmail.com>
2026-02-25 18:22:46 +0700

    Add files via upload

--- app/src/main/java/net/harimurti/tv/MainActivity.kt
+++ app/src/main/java/net/harimurti/tv/MainActivity.kt
@@ -3,6 +3,7 @@ package net.harimurti.tv
 import android.content.*
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.recyclerview.widget.GridLayoutManager
@@ -12,6 +13,8 @@ import net.harimurti.tv.adapter.ChannelAdapter
 import net.harimurti.tv.databinding.ActivityMainBinding
 import net.harimurti.tv.dialog.SearchDialog
 import net.harimurti.tv.dialog.SettingDialog
+import net.harimurti.tv.extension.SourcesReader
+import net.harimurti.tv.extra.Preferences
 import net.harimurti.tv.model.Channel
 import net.harimurti.tv.model.Playlist
 
@@ -19,23 +22,6 @@ open class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: CategoryAdapter
-
-    private val broadcastReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent) {
-            when (intent.getStringExtra(MAIN_CALLBACK)) {
-                UPDATE_PLAYLIST -> loadPlaylist()
-                INSERT_FAVORITE -> adapter.insertOrUpdateFavorite()
-                REMOVE_FAVORITE -> adapter.removeFavorite()
-            }
-        }
-    }
-
-    companion object {
-        const val MAIN_CALLBACK = "MAIN_CALLBACK"
-        const val UPDATE_PLAYLIST = "UPDATE_PLAYLIST"
-        const val INSERT_FAVORITE = "REFRESH_FAVORITE"
-        const val REMOVE_FAVORITE = "REMOVE_FAVORITE"
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,32 +40,53 @@ open class MainActivity : AppCompatActivity() {
         binding.buttonSettings.setOnClickListener { openSettings() }
         binding.buttonExit.setOnClickListener { finish() }
 
-        LocalBroadcastManager.getInstance(this)
-            .registerReceiver(broadcastReceiver, IntentFilter(MAIN_CALLBACK))
-
         loadPlaylist()
     }
 
     private fun loadPlaylist() {
-        val playlist = Playlist.cached
 
-        adapter = CategoryAdapter(playlist.categories)
+        binding.loading.visibility = View.VISIBLE
 
-        binding.rvCategory.adapter = adapter
-        binding.catAdapter = adapter
+        val playlistSet = Playlist()
 
-        adapter.setOnCategorySelected { category ->
-            showChannels(category.channels)
-        }
+        SourcesReader().set(Preferences().sources, object: SourcesReader.Result {
 
-        if (playlist.categories.isNotEmpty()) {
-            showChannels(playlist.categories[0].channels)
-        }
+            override fun onError(source: String, error: String) {}
+
+            override fun onResponse(playlist: Playlist?) {
+                if (playlist != null) playlistSet.mergeWith(playlist)
+            }
+
+            override fun onFinish() {
+
+                binding.loading.visibility = View.GONE
+
+                if (playlistSet.isCategoriesEmpty()) return
+
+                adapter = CategoryAdapter(playlistSet.categories)
+
+                binding.rvCategory.adapter = adapter
+                binding.catAdapter = adapter
+
+                adapter.setOnCategorySelected { category ->
+                    showChannels(category.channels)
+                }
+
+                // tampilkan kategori pertama
+                showChannels(playlistSet.categories[0].channels)
+            }
+        }).process(false)
     }
 
     private fun showChannels(channels: ArrayList<Channel>?) {
-        binding.rvChannels.adapter =
-            ChannelAdapter(channels, 0, false)
+
+        if (channels == null || channels.isEmpty()) return
+
+        binding.rvChannels.post {
+            binding.rvChannels.adapter =
+                ChannelAdapter(channels, 0, false)
+            binding.rvChannels.visibility = View.VISIBLE
+        }
     }
 
     private fun openSettings() {
@@ -88,11 +95,5 @@ open class MainActivity : AppCompatActivity() {
 
     private fun openSearch() {
         SearchDialog().show(supportFragmentManager.beginTransaction(), null)
-    }
-
-    override fun onDestroy() {
-        LocalBroadcastManager.getInstance(this)
-            .unregisterReceiver(broadcastReceiver)
-        super.onDestroy()
     }
 }
