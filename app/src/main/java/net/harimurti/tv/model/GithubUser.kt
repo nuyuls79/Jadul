1d26028bed6b0b983cf3de481d1909287ab21f8e
nuyuls79 <nuyulshare001@gmail.com>
2026-02-25 19:43:50 +0700

    clean repo

--- app/src/main/java/net/harimurti/tv/model/GithubUser.kt
+++
@@ -1,7 +0,0 @@
-package net.harimurti.tv.model
-
-class GithubUser {
-    lateinit var login: String
-    lateinit var url: String
-    var contributions = 0
-}
--- app/src/main/java/net/harimurti/tv/model/Release.kt
+++
@@ -1,8 +0,0 @@
-package net.harimurti.tv.model
-
-class Release {
-    var versionCode = 0
-    lateinit var versionName: String
-    lateinit var changelog: List<String>
-    var downloadUrl = ""
-}
