7b1e78024442ed3626f4e16a878a5abffcd1dc9f
nuyuls79 <nuyulshare001@gmail.com>
2026-02-28 08:34:00 +0700

    Add files via upload

--- app/src/main/java/net/harimurti/tv/model/Channel.kt
+++ app/src/main/java/net/harimurti/tv/model/Channel.kt
@@ -4,14 +4,30 @@ import com.google.gson.annotations.SerializedName
 
 class Channel {
     var name: String? = null
+    
     @SerializedName("logo_url")
     var logoUrl: String? = null
+    
     @SerializedName("stream_url")
     var streamUrl: String? = null
+    
     @SerializedName("drm_id")
     var drmId: String? = null
+    
     @SerializedName("user_agent")
     var userAgent: String? = null
+    
     @SerializedName("referer")
     var referer: String? = null
+    
+    // Tambahkan properti 'logo' untuk kompatibilitas dengan adapter
+    val logo: String?
+        get() = logoUrl
+    
+    // Tambahkan properti 'url' untuk kompatibilitas dengan adapter
+    val url: String?
+        get() = streamUrl
+    
+    // Tambahkan properti 'group' jika diperlukan (bisa diisi dari kategori)
+    var group: String? = null
 }
